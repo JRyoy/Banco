027632562c1316ac8ba521b68f8aeef47596ac8c
JRyoy <josbert0708@gmail.com>
2024-03-27 14:03:46 -0300

    Test

---
+++ TestCliente/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TestCliente;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ TestCuenta/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TestCuenta;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
