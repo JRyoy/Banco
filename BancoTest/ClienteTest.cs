0ae010ab480e9661491b671806a86a0d11806892
JRyoy <josbert0708@gmail.com>
2024-03-27 14:31:22 -0300

    jaja

--- BancoTest/ClienteTest.cs
+++ BancoTest/ClienteTest.cs
@@ -1,6 +1,6 @@
 namespace BancoTest;
 
-public class UnitTest1
+public class ClienteTest
 {
     [Fact]
     public void Test1()
---
+++ BancoTest/CuentaTest.cs
@@ -0,0 +1,5 @@
+namespace BancoTest;
+
+public class CuentaTest
+{
+}
